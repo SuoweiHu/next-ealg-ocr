d86a9c28d425be797ff1f9e71e9616b30fbd886d
SuoweiHu <suowei.h@gmail.com>
2025-03-20 10:34:49 +1100

    Feat: heroui tab to swtich between the upload-image and capture-image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,45 @@
+import { useState } from 'react';
+import { Tab } from '@headlessui/react';
 import OcrImageUpload from '@/components/ocr-image-upload';
 import OcrImageCapture from '@/components/ocr-image-capture';
 
 const IndexPage = () => {
   return (
-    <div style={{ padding: 10}}>
-      {/* <OcrImageUpload /> */}
-      <OcrImageCapture />
+    <div style={{ padding: 10 }}>
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+          <Tab
+            className={({ selected }: { selected: boolean }) =>
+              `w-full rounded-lg py-2.5 text-sm font-medium leading-5
+              ${selected
+                ? 'bg-white text-blue-700 shadow'
+                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+              }`
+            }
+          >
+            Upload Image
+          </Tab>
+          <Tab
+            className={({ selected }: { selected: boolean }) =>
+              `w-full rounded-lg py-2.5 text-sm font-medium leading-5
+              ${selected
+                ? 'bg-white text-blue-700 shadow'
+                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+              }`
+            }
+          >
+            Capture Image
+          </Tab>
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          <Tab.Panel>
+            <OcrImageUpload />
+          </Tab.Panel>
+          <Tab.Panel>
+            <OcrImageCapture />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
